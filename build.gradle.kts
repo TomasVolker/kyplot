ecdca63a23b43afd26dcc87c03f264b55b0c9501
tomasvolker <tomasvolker@gmail.com>
2019-03-26 21:09:35 +0100

    Update project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,20 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    java
-    kotlin("jvm") version "1.3.0-rc-146"
+    kotlin("jvm") version "1.3.21"
+    maven
 }
 
-group = "perchanegro"
-version = "0.1"
+group = "tomasvolker"
+version = "0.1.0"
 
 repositories {
 
     mavenCentral()
     jcenter()
 
-    maven { url = uri("http://dl.bintray.com/kotlin/kotlin-eap") }
-
     maven { url = uri("http://dl.bintray.com/kyonifer/maven") }
+    maven { url = uri("http://dl.bintray.com/tomasvolker/maven") }
+    maven { url = uri("https://jitpack.io") }
 
 }
 
@@ -24,16 +23,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("junit", "junit", "4.12")
 
+    implementation(group = "tomasvolker", name = "numeriko-core", version = "0.0.3")
+    implementation(group = "com.github.tomasvolker", name = "kyscript", version = "0.1.0")
+
     testImplementation(group = "com.kyonifer", name = "koma-core-ejml", version = "0.12")
 
-    implementation(files("libs/kyscript-0.1.jar"))
-    implementation(files("libs/kyscript-0.1-sources.jar"))
-
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
